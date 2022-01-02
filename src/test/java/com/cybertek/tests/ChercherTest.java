803869aaa371e1899f4cb9f898d6dda5a832d16d
Rotopu16 <rotopu@gmail.com>
2022-01-02 18:40:36 +0300

    Alert test completed

--- src/test/java/com/cybertek/tests/ChercherTest.java
+++ src/test/java/com/cybertek/tests/ChercherTest.java
@@ -1,7 +1,12 @@
 package com.cybertek.tests;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,6 +16,7 @@ import java.util.concurrent.TimeUnit;
 public class ChercherTest {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeMethod
     public void setUp(){
@@ -29,7 +35,16 @@ public class ChercherTest {
 
     @Test
 
-    public void alertPresentTest(){}
+    public void alertPresentTest(){
+        WebElement initiateAlert = driver.findElement(By.id("alert"));
+        initiateAlert.click();
+        wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.alertIsPresent());
+        //hande JS alert
+        //NoAlertPresentException
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+    }
 
 
 }
