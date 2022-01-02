022e8f15f65b3b7127199ae8c95cedbab3a64cb2
Rotopu16 <rotopu@gmail.com>
2022-01-02 19:20:21 +0300

    Sec test completed

--- src/test/java/com/cybertek/tests/ChercherTest.java
+++ src/test/java/com/cybertek/tests/ChercherTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -44,6 +45,22 @@ public class ChercherTest {
         //NoAlertPresentException
         Alert alert = driver.switchTo().alert();
         alert.accept();
+    }
+
+    @Test
+    public void disabledButtonTest(){
+        WebElement button = driver.findElement(By.id("disable"));
+        System.out.println("button.isEnabled() = " + button.isEnabled());//false
+
+        WebElement buttonInitiator = driver.findElement(By.id("enable-button"));
+        buttonInitiator.click();
+
+        wait= new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+
+        System.out.println("button.isEnabled() = " + button.isEnabled());
+
+        Assert.assertTrue(button.isEnabled(), "Verify button is enanbled");
     }
 
 
