01f3565c3e77a00d03accc4a1ddfb398c08d65b4
Rotopu16 <rotopu@gmail.com>
2022-01-02 18:28:12 +0300

    Initial Commit

---
+++ src/test/java/com/cybertek/tests/ChercherTest.java
@@ -0,0 +1,35 @@
+package com.cybertek.tests;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ChercherTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        //implicitly wait
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+
+    public void alertPresentTest(){}
+
+
+}
